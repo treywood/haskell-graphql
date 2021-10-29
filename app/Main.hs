5678eea89358c97a142c1867f0e1a3910051f27a
Edwin Wood <edwinjwood3rd@gmail.com>
2021-10-29 09:53:44 -0600

    some refactor

--- app/Main.hs
+++ app/Main.hs
@@ -48,8 +48,9 @@ instance Named NamedType where
   getName (NamedPerson p) = getName p
   getName (NamedPet p)    = getName p
 
-namedInterface :: (Named a) => Interface a
-namedInterface = Interface "Named" [Field "name" StringType (return . getName)]
+hasNameInterface :: (Named a) => Interface a
+hasNameInterface =
+  Interface "HasName" [Field "name" StringType (return . getName)]
 
 genderType = EnumType "Gender" [MALE, FEMALE]
 
@@ -59,7 +60,7 @@ personType :: SchemaType Person
 personType =
   ObjectType
     "Person"
-    [namedInterface]
+    [hasNameInterface]
     [ Field "age" IntType (return . age)
     , Field "gender" genderType (return . gender)
     , Field "spouse" (NullableType personType) (return . spouse)
@@ -69,33 +70,8 @@ petType :: SchemaType Pet
 petType =
   ObjectType
     "Pet"
-    [namedInterface]
+    [hasNameInterface]
     [Field "specie" specieType (\(Pet specie _) -> return specie)]
-
-trey = Person {name = "Trey", age = 31, gender = MALE, spouse = Just debbie}
-
-debbie = Person {name = "Debbie", age = 32, gender = FEMALE, spouse = Just trey}
-
-getPets :: () -> IO [Pet]
-getPets () = do
-  contents <- readFile "pets.txt"
-  return $ map read (lines contents)
-
-getPeople :: () -> IO (Maybe [Person])
-getPeople () = do
-  random <- (randomIO :: IO Float)
-  return $
-    if random > 0.5
-      then Just [trey, debbie]
-      else Nothing
-
-getNamed :: () -> IO NamedType
-getNamed () = do
-  random <- (randomIO :: IO Float)
-  return $
-    if random > 0.5
-      then NamedPerson trey
-      else NamedPet (Pet DOG "Murphy")
 
 queryType :: SchemaType ()
 queryType =
@@ -104,8 +80,30 @@ queryType =
     []
     [ Field "people" (NullableType (ListType personType)) getPeople
     , Field "pets" (ListType petType) getPets
-    , Field "named" (InterfaceType namedInterface) getNamed
+    , Field "named" (InterfaceType hasNameInterface) getNamed
     ]
+  where
+    getPets :: () -> IO [Pet]
+    getPets () = do
+      contents <- readFile "pets.txt"
+      return $ map read (lines contents)
+    getPeople :: () -> IO (Maybe [Person])
+    getPeople () = do
+      random <- (randomIO :: IO Float)
+      return $
+        if random > 0.5
+          then Just [trey, debbie]
+          else Nothing
+    getNamed :: () -> IO NamedType
+    getNamed () = do
+      random <- (randomIO :: IO Float)
+      return $
+        if random > 0.5
+          then NamedPerson trey
+          else NamedPet (Pet DOG "Murphy")
+    trey = Person {name = "Trey", age = 31, gender = MALE, spouse = Just debbie}
+    debbie =
+      Person {name = "Debbie", age = 32, gender = FEMALE, spouse = Just trey}
 
 main :: IO ()
 main = do
