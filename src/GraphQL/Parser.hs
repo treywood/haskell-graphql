3c513bcdf87f3f7cd1e781898b047aeaf02eeec2
Trey Wood <treyw@squareup.com>
2022-02-13 13:31:35 -0700

    point-free

--- src/GraphQL/Parser.hs
+++ src/GraphQL/Parser.hs
@@ -1,14 +1,11 @@
 module GraphQL.Parser
   ( parseQuery
-  , ParseResult
   ) where
 
-import           GraphQL.Core            (Query (..))
+import           Data.Either.Combinators
+import           GraphQL.Core
 import           GraphQL.Parser.Internal
 import           Text.Megaparsec
 
 parseQuery :: String -> ParseResult [Query]
-parseQuery queryStr =
-  case parse queryParser "" queryStr of
-    Right result -> Right result
-    Left err     -> Left (errorBundlePretty err)
+parseQuery = mapLeft errorBundlePretty . parse queryParser ""
